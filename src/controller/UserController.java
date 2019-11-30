0d6adebbad57452e77d43b69d3f55a6118845312
My <502914391@qq.com>
2019-11-30 22:29:14 +0800

    删除多余的jsp
    (cherry picked from commit 93624bbcccb2a8e446969553d151b480367b7875)

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -43,7 +43,7 @@ public class UserController {
     }
 
     @RequestMapping("userheadUpload.action")
-    public String userheadUpload() {
+    public String userHeadUpload() {
         return "upload";
     }
 
